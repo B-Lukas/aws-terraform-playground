c786d136a555780f0f75f64a407a92c6c47a707d
Lukas Biernat <lukas.biernat@pexon.-consulting.de>
2023-01-20 11:09:27 +0100

    Add most of lambda function components to terraform

--- aws/terraform/lambda.tf
+++ aws/terraform/lambda.tf
@@ -1,3 +1,56 @@
+resource "aws_vpc" "main" {
+  cidr_block       = "172.31.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "main"
+  }
+}
+
+resource "aws_subnet" "primary" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "172.31.0.0/24"
+
+  tags = {
+    Name = "Primary"
+  }
+}
+
+resource "aws_subnet" "secondary" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "172.31.1.0/24"
+
+  tags = {
+    Name = "Secondary"
+  }
+}
+
+resource "aws_security_group" "allow_https" {
+  name        = "allow_https"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = [aws_vpc.main.cidr_block]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_https"
+  }
+}
+
 resource "aws_lambda_function" "HelloWorld" {
   function_name = "HelloWorld"
   role          = "arn:aws:iam::363529221899:role/service-role/HelloWorld-role-5gvipgn6"
@@ -10,7 +63,29 @@ resource "aws_lambda_function" "HelloWorld" {
   }
   timeout = 15
   vpc_config {
-     security_group_ids = ["sg-01972fdbdf92efebc"]
-     subnet_ids = ["subnet-034f59074b7c0cf46", "subnet-04bfd92a49a800fc2", "subnet-088328f7fc9f48af5"]
+     security_group_ids = [aws_security_group.allow_https.id]
+     subnet_ids = [aws_subnet.primary.id, aws_subnet.secondary.id]
+  }
+}
+
+# This is the trigger of the Lambda function
+resource "aws_s3_bucket_notification" "s3_notification" {
+  bucket = "lubi-lambda-test-bucket"
+
+  lambda_function {
+    lambda_function_arn = aws_lambda_function.HelloWorld.arn
+    events              = ["s3:ObjectCreated:Put"]
+  }
+}
+
+# This is the destination of the Lambda function
+resource "aws_lambda_function_event_invoke_config" "sns_destination" {
+  function_name = aws_lambda_function.HelloWorld.function_name
+  maximum_retry_attempts = 0
+
+  destination_config {
+    on_success {
+      destination = "arn:aws:sns:eu-central-1:363529221899:lambda-test.fifo"
+    }
   }
 }
